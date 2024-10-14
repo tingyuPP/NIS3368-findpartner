d23a822ab79888836abfabd9d37e62ed59befc06
caoyifan <caoyifan0629@sjtu.edu.cn>
2024-10-14 15:48:43 +0800

    database api

--- app01/database.py
+++ app01/database.py
@@ -15,7 +15,24 @@ def check_user_database(id):
     user = 0
     return user
 
-# 查看需求，输入需求id，返回需求类
+# 查看需求，输入需求id，返回需求类，没有查询到则返回-1
+def check_notice_database(id):
+    notice = 0
+    return notice
+
+# 检索需求，输入检索type，检索内容，返回检索结果（需求列表）
+# 这部分可以和check_user_database一起实现，但是通过id检索需求会经常调用，所以最好分开实现
+def search_notice_database(type, notice_content):
+    notice_list = []
+    return notice_list
+
+# 修改用户信息，输入用户id、用户修改后的内容，把该id下的内容全部用user_content的内容替换，返回是否修改成功
+def change_user_database(id, user_content):
+    if_success = 0
+    return if_success
 
 
-# 修改用户信息，
+# 修改需求信息
+def change_notice_database(id, notice_content):
+    if_success = 0
+    return if_success
