8ca0377e8ecda5a09b81c2c9dbd1750d344f4565
caoyifan <caoyifan0629@sjtu.edu.cn>
2024-10-14 15:31:32 +0800

    move function.py and function_class.py, add database.py

---
+++ app01/database.py
@@ -0,0 +1,21 @@
+from function_class import *
+
+# 创建用户，无输入，返回user id
+def create_user():
+    id = 0
+    return id
+
+# 创建需求，无输入，返回notice id
+def create_notice():
+    id = 0
+    return id
+
+# 查看用户，输入用户id，返回用户类，没有查询到则返回-1
+def check_user_database(id):
+    user = 0
+    return user
+
+# 查看需求，输入需求id，返回需求类
+
+
+# 修改用户信息，
