878eb87f4baa619c2b172ca483cd809132ef6533
Liang Tongyu <995572795@qq.com>
2024-10-28 19:12:43 +0800

    Update sports.js

--- static/js/waterfallgetcards/sports.js
+++ static/js/waterfallgetcards/sports.js
@@ -1,4 +1,4 @@
-function initializeWaterfall_recommend() {
+function initializeWaterfall_sports() {
   // 使用 fetch 或 jQuery 的 AJAX 请求后端数据
   fetch("/get-sports-notice/")
     .then((response) => response.json())
