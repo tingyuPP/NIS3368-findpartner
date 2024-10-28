a07de3a64793c964e8d700707b52b8b4e34d7b52
Liang Tongyu <995572795@qq.com>
2024-10-28 19:12:59 +0800

    Update study.js

--- static/js/waterfallgetcards/study.js
+++ static/js/waterfallgetcards/study.js
@@ -1,4 +1,4 @@
-function initializeWaterfall_recommend() {
+function initializeWaterfall_study() {
   // 使用 fetch 或 jQuery 的 AJAX 请求后端数据
   fetch("/get-study-notice/")
     .then((response) => response.json())
