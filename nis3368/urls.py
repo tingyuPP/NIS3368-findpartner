9447d176efbb2b8ad26b21d61915f9ab0ad7fcef
caoyifan <caoyifan0629@sjtu.edu.cn>
2024-10-31 09:12:10 +0800

    update apply

--- nis3368/urls.py
+++ nis3368/urls.py
@@ -58,10 +58,10 @@ urlpatterns = [
     path("get-my-published-notice/", views.get_my_published_notice),
     path("get-my-replied-notice/", views.get_my_replied_notice),
     path("api/request_notice", views.request_notice_view, name="request_notice"),
-    path("applylist/<int:post_id>/", views.applylist, name="applylist"),
-    # path("answer_request/", views.handle_answer_request, name="answer_request"),
+    # path("applylist/<int:post_id>/", views.applylist, name="applylist"),
+    path("answer_request/", views.handle_answer_request, name="answer_request"),
 
     path("api/disable_notice", views.disable__notice),
     path("api/recover_notice", views.recover_notice),
-    path("applylist/<int:post_id>", views.applylist, name="applylist"),
+    path("applylist/<int:post_id>/", views.applylist, name="applylist"),
 ]
